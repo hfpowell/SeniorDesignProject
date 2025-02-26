aee3e499524945a09ca51106c93c86f1441e374d
Harrison Powell <hfpowell1@gmail.com>
2025-02-26 16:44:55 -0600

    Fixed the MATLAB so now it works how we'll need with our design, with array shifting and correct array values

--- basic_waveform_checking.c
+++ basic_waveform_checking.c
@@ -8,18 +8,46 @@
 
 
 //the graphs will be translated into simple 1s and 0s
-int expected_waveform[1000];
+/*int expected_waveform[1000];
 int user_input[1000];
 
-int score = 0;
+//populate expected array
 
-for(int i = 0; i < 1000; i++)
-{
-	if(expected_waveform[i] == user_input[i])
-	{
-		score++;
-	}
-}
+*/
+fig = figure;
+set(fig, 'WindowButtonDownFcn', @(~,~) disp('Click detected')); % Dummy function to register clicks
+
+% Initialize the 10x100 matrix
+data = zeros(10, 100);
+
+for row = 1:10  % 10 seconds (1 row per second)
+    tic; % Start timing for the second
+    
+    for col = 1:100  % 100 samples per second
+        % Check if the mouse is clicked
+        clickState = strcmp(get(fig, 'SelectionType'), 'normal'); % 1 if clicked, 0 if not
+        
+        % Store the value
+        data(row, col) = clickState;
+        
+        % Ensure 10ms delay to get 100 samples per second
+        while toc < (col / 100)
+            pause(0.0001); % Fine control to maintain timing
+        end
+    end
+    
+    % Reset the SelectionType to avoid persistent detection
+    set(fig, 'SelectionType', 'open');
+end
+
+% Close the figure after data collection
+close(fig);
+
+% Display the collected data
+disp(data);
+
+
+
 
 
 
@@ -27,8 +55,9 @@ for(int i = 0; i < 1000; i++)
 
 //the following program looks for groups of 1s that indicate high points on the graph
 
-
+/*
 int[] increaseTollerance(int[] waveform)
 {
 	for(int i = 0; i < waveform.c
 }
+*/
